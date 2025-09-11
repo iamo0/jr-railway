2fde683f1e9f94b520c441c02582666c60250ff5
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-09-11 20:29:18 +0300

    Connect header styles add css-module

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,21 +1,21 @@
-import "./header.css";
+import styles from "./header.module.css";
 
 import Link from "next/link";
 import Image from "next/image";
 import BurgerMenu from "../burger-menu/burder-menu";
 
 export default function Header() {
-  return <header className="header">
+  return <header className={styles.header}>
     <Link href="/">
       <Image src="logo.svg" alt="Railway" width={73} height={51} />
     </Link>
     <BurgerMenu>
-      <nav className="header-nav">
-        <ul className="header-nav-list">
-          <li className="header-nav-list-item"><Link href="/mobile" className="header-nav-list-link">Mobile App</Link></li>
-          <li className="header-nav-list-item"><Link href="/contact" className="header-nav-list-link">Contact</Link></li>
-          <li className="header-nav-list-item"><Link href="/faq" className="header-nav-list-link">FAQs</Link></li>
-          <li className="header-nav-list-item"><Link href="/signup" className="header-nav-list-link">Sign Up</Link></li>
+      <nav className={styles.headerNav}>
+        <ul className={styles.headerNavList}>
+          <li className={styles.headerNavListItem}><Link href="/mobile" className={styles.headerNavListLink}>Mobile App</Link></li>
+          <li className={styles.headerNavListItem}><Link href="/contact" className={styles.headerNavListLink}>Contact</Link></li>
+          <li className={styles.headerNavListItem}><Link href="/faq" className={styles.headerNavListLink}>FAQs</Link></li>
+          <li className={styles.headerNavListItem}><Link href="/signup" className={styles.headerNavListLink}>Sign Up</Link></li>
         </ul>
       </nav>
     </BurgerMenu>
