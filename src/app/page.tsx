6783cd8f00747703ab070f6d2e70a1ecdfef03e0
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-09-12 03:56:34 +0300

    Add radio button switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,27 @@
+import LabelledRadioButton from "@/components/labelled-radio-button/labelled-radio-button";
 import styles from "./page.module.css";
 
 export default function Home() {
   return <>
     <h1 className={styles.pageHeader}>Search Results</h1>
     <form className={styles.search}>
-      <fieldset className={`${styles.searchField}`}>
-        
+      <fieldset className={`${styles.searchField} ${styles.searchFieldInline}`}>
+        <LabelledRadioButton id="search-type-one-way" name="search-type" value="one-way" defaultChecked>One Way</LabelledRadioButton>
+        <LabelledRadioButton id="search-type-round-trip" name="search-type" value="round-trip">Round Trip</LabelledRadioButton>
       </fieldset>
 
       <fieldset className={styles.searchField}>
-        <label>Departure</label>
+        <label className={styles.searchFieldLabel}>Departure</label>
         <input placeholder="Your City/Station" type="text" id="search-departure" name="search-departure" />
       </fieldset>
 
       <fieldset className={styles.searchField}>
-        <label>Arrival</label>
+        <label className={styles.searchFieldLabel}>Arrival</label>
         <input placeholder="Where To?" type="text" id="search-arrival" name="search-arrival" />
       </fieldset>
 
       <fieldset className={styles.searchField}>
-        <label>Pick Your Lucky Day</label>
+        <label className={styles.searchFieldLabel}>Pick Your Lucky Day</label>
         <input placeholder="Depart" type="date" id="search-departure-date" name="search-departure-date" />
         <input placeholder="Return" type="date" id="search-arrival-date" name="search-arrival-date" />
       </fieldset>
---
+++ src/components/labelled-radio-button/labelled-radio-button.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import styles from "./labelled-radio-button.module.css";
+
+import { PropsWithChildren } from "react";
+
+interface LabelledRadioButtonProps extends React.InputHTMLAttributes<HTMLInputElement>, PropsWithChildren {
+  id: string,
+}
+
+export default function LabelledRadioButton({
+  children,
+  defaultChecked,
+  name,
+  value,
+  id,
+}: LabelledRadioButtonProps) {
+  return <>
+    <input
+      type="radio" id={id} name={name} value={value} defaultChecked={defaultChecked}
+      className={styles.radio}
+    />
+    <label htmlFor={id} className={styles.label}>{children}</label>
+  </>;
+}
