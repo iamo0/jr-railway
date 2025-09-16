76291e3f25d5665dea3150f054ef44dfb8e4b165
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-09-16 03:19:14 +0300

    Fix numeric input by adding possibility to use value. And add onChange handler

--- src/components/numeric-input/numeric-input.tsx
+++ src/components/numeric-input/numeric-input.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./numeric-input.module.css";
-import useClamped from "@/hooks/use-clamped";
+import clamp from "@/helpers/clamp";
 
-interface NumericInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface NumericInputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
   id: string,
   step: number,
   min: number,
   max: number,
-  defaultValue: number,
+  defaultValue?: number,
+  value?: number,
+  onChange?: (value: number) => void,
 }
 
 export default function NumericInput(props: NumericInputProps) {
@@ -18,14 +21,22 @@ export default function NumericInput(props: NumericInputProps) {
     step = 1,
     min = -Infinity,
     max = Infinity,
+    onChange = () => {},
+    value,
     ...rest
   } = props;
 
-  const [value, setValue] = useClamped(defaultValue, min, max);
+  const [currentValue, setValue] = useState(value || defaultValue);
+
+  function setClampedValue(newValue: number) {
+    const clampedValue = clamp(newValue, min, max);
+    setValue(clampedValue);
+    onChange(clampedValue);
+  }
 
   return <div className={`${styles.numericInput} ${className ?? ""}`}>
-    <input className={styles.input} type="number" step={step} min={min} max={max} {...rest} value={value} readOnly />
-    <button disabled={value === min} type="button" onClick={() => setValue(value - step)} className={`${styles.controlButton} ${styles.controlButtonDecrease}`}>Decrease</button>
-    <button disabled={value === max} type="button" onClick={() => setValue(value + step)} className={`${styles.controlButton} ${styles.controlButtonIncrease}`}>Increase</button>
+    <input className={styles.input} type="number" step={step} min={min} max={max} {...rest} value={currentValue} readOnly />
+    <button disabled={currentValue === min} type="button" onClick={() => setClampedValue(currentValue - step)} className={`${styles.controlButton} ${styles.controlButtonDecrease}`}>Decrease</button>
+    <button disabled={currentValue === max} type="button" onClick={() => setClampedValue(currentValue + step)} className={`${styles.controlButton} ${styles.controlButtonIncrease}`}>Increase</button>
   </div>;
 };
