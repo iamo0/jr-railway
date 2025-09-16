6a7a2f971d61e2f01eae53ec3fa9f5069fab1a3e
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-09-16 03:16:13 +0300

    Add the rest of reducers and form validation

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -4,7 +4,7 @@ import styles from "./search-form.module.css";
 
 import LabelledRadioButton from "../labelled-radio-button/labelled-radio-button";
 import NumericInput from "../numeric-input/numeric-input";
-import { useReducer } from "react";
+import { useMemo, useReducer } from "react";
 
 enum TravelType {
   ONE_WAY = 1,
@@ -17,25 +17,34 @@ type SearchData = {
   departure: string,
   arrival: string,
   dateDeparture: number, // Всегда больше чем Date.now()
-  dateArrival: number, // Только для ROUND_TRIP. Всегда больше чем dateDeparture
+  dateArrival: number, // Только дляROUND_TRIP. Всегда больше чем dateDeparture
 };
 
-type SearchDataReducerAction = {
-  type: "SET_TRAVEL_TYPE",
-  payload: TravelType,
-};
+type SearchDataReducerAction =
+  | { type: "SET_TRAVEL_TYPE", payload: TravelType }
+  | { type: "SET_PASSENGERS", payload: number }
+  | { type: "SET_DEPARTURE", payload: string }
+  | { type: "SET_ARRIVAL", payload: string }
+  | { type: "SET_DATE_DEPARTURE", payload: number }
+  | { type: "SET_DATE_ARRIVAL", payload: number };
 
 function searchDataReducer(state: SearchData, {type, payload}: SearchDataReducerAction): SearchData {
   switch(type) {
-    case "SET_TRAVEL_TYPE": {
-      return {
-        ...state,
-        travelType: payload,
-      };
-    }
+    case "SET_TRAVEL_TYPE":
+      return { ...state, travelType: payload };
+    case "SET_PASSENGERS":
+      return { ...state, passengers: payload };
+    case "SET_DEPARTURE":
+      return { ...state, departure: payload };
+    case "SET_ARRIVAL":
+      return { ...state, arrival: payload };
+    case "SET_DATE_DEPARTURE":
+      return { ...state, dateDeparture: payload };
+    case "SET_DATE_ARRIVAL":
+      return { ...state, dateArrival: payload };
+    default:
+      throw new TypeError("Unknown action");
   }
-
-  throw new TypeError("Unknown action");
 };
 
 export default function SearchForm() {
@@ -47,6 +56,20 @@ export default function SearchForm() {
     dateDeparture: Date.now(),
     dateArrival: Date.now() + 1000 * 60 * 60 * 24,
   });
+
+  const validationMessages = useMemo(function() {
+    return [
+      () => searchData.passengers < 0 ? "Too few passengers" : null,
+      () => searchData.passengers > 10 ? "Too many passengers" : null,
+      () => searchData.departure.trim() === "" ? "Departure place is not set" : null,
+      () => searchData.arrival.trim() === "" ? "Travel destination is not set" : null,
+      () => searchData.arrival === searchData.departure && searchData.arrival.trim() !== "" ? "Destination couldn't be the same place as departure place" : null,
+      () => searchData.dateDeparture && searchData.dateDeparture < Date.now() ? "Departure date is in the past" : null,
+      () => searchData.travelType === TravelType.ROUND_TRIP && searchData.dateArrival <= searchData.dateDeparture ? "Arrival date should be after departure date" : null,
+    ]
+      .map((f) => f())
+      .filter(m => m);
+  }, [searchData]);
 
   return <form className={styles.search} method="GET">
     <fieldset className={`${styles.searchField} ${styles.searchFieldInline}`}>
@@ -79,7 +102,11 @@ export default function SearchForm() {
         id="search-passengers"
         name="search-passengers"
         step={1} min={1} max={10}
-        defaultValue={searchData.passengers}
+        value={searchData.passengers}
+        onChange={(value) => dispatch({
+          type: "SET_PASSENGERS",
+          payload: value,
+        })}
       />
     </fieldset>
 
@@ -91,7 +118,11 @@ export default function SearchForm() {
         type="text"
         id="search-departure"
         name="search-departure"
-        defaultValue={searchData.departure}
+        value={searchData.departure}
+        onChange={(evt) => dispatch({
+          type: "SET_DEPARTURE",
+          payload: evt.target.value,
+        })}
       />
     </fieldset>
 
@@ -103,7 +134,11 @@ export default function SearchForm() {
         type="text"
         id="search-arrival"
         name="search-arrival"
-        defaultValue={searchData.arrival}
+        value={searchData.arrival}
+        onChange={(evt) => dispatch({
+          type: "SET_ARRIVAL",
+          payload: evt.target.value,
+        })}
       />
     </fieldset>
 
@@ -116,6 +151,11 @@ export default function SearchForm() {
         type="date"
         id="search-departure-date"
         name="search-departure-date"
+        value={new Date(searchData.dateDeparture).toISOString().slice(0, 10)}
+        onChange={(evt) => dispatch({
+          type: "SET_DATE_DEPARTURE",
+          payload: new Date(evt.target.value).getTime(),
+        })}
       />
 
       <input
@@ -125,9 +165,22 @@ export default function SearchForm() {
         id="search-arrival-date"
         name="search-arrival-date"
         disabled={searchData.travelType === TravelType.ONE_WAY}
+        value={new Date(searchData.dateArrival).toISOString().slice(0, 10)}
+        onChange={(evt) => dispatch({
+          type: "SET_DATE_ARRIVAL",
+          payload: new Date(evt.target.value).getTime(),
+        })}
       />
     </fieldset>
 
-    <button type="submit" className={styles.searchSubmit}>Ticket, Please!</button>
+    <button disabled={validationMessages.length > 0} type="submit" className={styles.searchSubmit}>Ticket, Please!</button>
+
+    {
+      validationMessages.length === 0
+        ? null
+        : <ul className={styles.validationMessages}>
+          {validationMessages.map((m, i) => <li className={styles.validationMessage} key={`message-${i}`}>{m}</li>)}
+        </ul>
+    }
   </form>;
 };
