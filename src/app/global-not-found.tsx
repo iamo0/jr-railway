400b2260a7dfe424e07662917eec5285c51cf1d1
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-10-23 19:16:45 +0300

    Add custom global 404 page

---
+++ src/app/global-not-found.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+
+const poppinsFont = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: "Railway",
+  description: "Official tickets for Indian Railways",
+};
+
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body className={`${poppinsFont.variable}`}>
+        <main className="main">
+          <h1>Такой страницы нет!</h1>
+          <p>Попробуйте <Link href="/">сначала</Link></p>
+        </main>
+      </body>
+    </html>
+  );
+}
