c48920b4ce4d3825d67fcb9751a930ad80af0766
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-10-23 20:28:47 +0300

    Search form destination input: change datalist dynamically

--- src/components/search-form/input-with-suggest/input-with-suggest.tsx
+++ src/components/search-form/input-with-suggest/input-with-suggest.tsx
@@ -1,7 +1,15 @@
-import { HTMLAttributes } from "react";
+"use client";
+
+import { ChangeEvent, HTMLAttributes, useState } from "react";
 
 interface DataListOption {
   value: string,
+  priority?: true,
+}
+
+interface OptionsState {
+  state: "loading" | "idle",
+  data: DataListOption[],
 }
 
 interface InputWithSuggestProps extends HTMLAttributes<HTMLInputElement> {
@@ -16,22 +24,53 @@ interface InputWithSuggestProps extends HTMLAttributes<HTMLInputElement> {
 export default function InputWithSuggest(props: InputWithSuggestProps) {
   const {
     defaultOptions = [
-      { value: "Москва" },
-      { value: "Санкт Петербург" },
-      { value: "Нижний Новгород" },
+      { value: "Москва", priority: true, },
+      { value: "Мурино" },
+      { value: "Санкт Петербург", priority: true, },
+      { value: "Нижнекамск" },
+      { value: "Нижневартовск" },
+      { value: "Нижний Тагил" },
+      { value: "Нижний Новгород", priority: true, },
+      { value: "Владивосток", priority: true, },
       { value: "Вышний Волочек" },
-      { value: "Пекин" },
-      { value: "Берлин" },
-      { value: "Лондон" },
+      { value: "Пекин", priority: true, },
+      { value: "Берлин", priority: true, },
+      { value: "Биробиджан" },
+      { value: "Лондон", priority: true, },
     ],
     listId,
     ...rest
   } = props;
 
+  const [inputValue, setInputValue] = useState("");
+
+  const [optionsState, setOptionsState] = useState<OptionsState>({
+    state: "idle",
+    data: defaultOptions.filter((o) => o.priority),
+  });
+
+  function handleInputChange(evt: ChangeEvent<HTMLInputElement>) {
+    const value = evt.target.value;
+    setInputValue(value);
+
+    if (value.length < 2) {
+      setOptionsState({
+        state: "idle",
+        data: defaultOptions.filter((o) => o.priority),
+      })
+      return;
+    }
+
+    setOptionsState({
+      state: "idle",
+      data: [...defaultOptions],
+    });
+  }
+
   return <>
-    <input autoComplete="off" list={listId} {...rest} />
+    <input autoComplete="off" list={listId} {...rest} value={inputValue} onChange={handleInputChange} />
     <datalist id={listId}>
-      {defaultOptions.map(({value}) => <option value={value} key={value.replaceAll(" ", "-")} />)}
+      {optionsState.data.map(({value}) => <option value={value} key={value.replaceAll(" ", "-")} />)}
     </datalist>
   </>;
 }
